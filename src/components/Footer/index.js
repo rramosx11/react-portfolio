a07b1cb7240566e768c17280edfd67c35420af78
rramosx11 <rodolforamosd11@gmail.com>
2022-10-30 18:32:42 -0400

    github added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { VscGithubAlt } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <h2>Made by Rodolfo Ramos ©</h2>
+      <p>
+        <ul>
+          <a href="https://github.com/rramosx11">
+            <li className="logo">
+              <VscGithubAlt />
+            </li>
+          </a>
+        </ul>
+      </p>
+    </footer>
   );
 }
 
