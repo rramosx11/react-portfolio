5614995c1bb26fc95c543a61e0687c5728e0f3cf
rramosx11 <rodolforamosd11@gmail.com>
2022-12-04 23:11:40 -0500

    added the github repo links as github logos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ function Header(props) {
       <section>
         <header className="flex-row px-1">
           <div>
-            <h1>Rodolfo Ramos's Portfolio</h1>
+            <h1>Rodolfo Ramos</h1>
           </div>
           <div>
             <Nav
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,8 @@ import pawfectFriendsImage from "../../assets/small/pawfect-friends.png";
 import noteTakerImage from "../../assets/small/Note-Taker Screenshot.png";
 import eventFinderImage from "../../assets/small/Event Finder.png";
 import weatherDashboardImage from "../../assets/small/Weather Dashboard 2.png";
+import playerUpImage from "../../assets/small/player-up.png";
+import { VscGithubAlt } from "react-icons/vsc";
 
 function Portfolio() {
   return (
@@ -13,6 +15,40 @@ function Portfolio() {
           <h2 className="section-title secondary-border">Work</h2>
         </div>
 
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://playerup2022.herokuapp.com/">
+                {" "}
+                <img
+                  src={playerUpImage}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Player Up Website "
+                />
+              </a>
+            </div>
+
+            <div className="job-text">
+              <h4>
+                Player Up
+                <a href="https://github.com/rramosx11/player-up">
+                  <VscGithubAlt />
+                </a>
+              </h4>
+              <p>
+                Player UP is a social application designed to organize people
+                with a similar interest - in this case games of all kinds! A
+                user can signup for a user account. When logged in they can
+                browse local activities that are available to sign up to
+                participate in, sign up for activities, and add an event for
+                others to join. This is a MERN stack application composed of a
+                REACT front end, MongoDB database, and Node.js/Express.js server
+                and API with a GraphQL API built with Apollo Server.
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="job">
           <div className="job-info">
             <div className="job-img">
@@ -27,7 +63,12 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Pawfect Friends</h4>
+              <h4>
+                Pawfect Friends
+                <a href="https://github.com/rramosx11/Pawfect-Friends">
+                  <VscGithubAlt />
+                </a>
+              </h4>
               <p>
                 A web application that uses Node and Express back end and uses
                 both GET and POST routes for creating and adding new social
@@ -44,7 +85,7 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="https://jessicam04.github.io/wheres-the-band/">
+              <a href="https://rramosx11.github.io/wheres-the-band/ ">
                 {" "}
                 <img
                   src={eventFinderImage}
@@ -55,7 +96,12 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Event Finder</h4>
+              <h4>
+                Event Finder
+                <a href="https://github.com/rramosx11/wheres-the-band">
+                  <VscGithubAlt />
+                </a>
+              </h4>
               <p>
                 Application that uses two server-side APIs (Google Maps
                 Directions and Seatgeek) to get directions from the starting
@@ -79,7 +125,12 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Code Quiz</h4>
+              <h4>
+                Code Quiz
+                <a href="https://github.com/rramosx11/code-quiz">
+                  <VscGithubAlt />
+                </a>
+              </h4>
               <p>
                 A javascript and local storage appliaction that will let you
                 take a quiz on basic coding questions.{" "}
@@ -102,7 +153,12 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Note Taker</h4>
+              <h4>
+                Note Taker
+                <a href="https://github.com/rramosx11/Text-Editor">
+                  <VscGithubAlt />
+                </a>
+              </h4>
               <p>
                 An express application that provides the user the ability to add
                 new notes, and to delete notes.{" "}
@@ -125,7 +181,12 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Weather Dashboard</h4>
+              <h4>
+                Weather Dashboard
+                <a href="https://github.com/rramosx11/Weather-Dashboard">
+                  <VscGithubAlt />
+                </a>
+              </h4>
               <p>
                 A website that allows the user see the weather outlook for
                 multiple cities. The user can enter a city and see a 5-Day
